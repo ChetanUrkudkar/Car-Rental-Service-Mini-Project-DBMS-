ec87d65c4463198a1c52f3bcfb533d945bb74bb4
ChetanUrkudkar <84468525+ChetanUrkudkar@users.noreply.github.com>
2021-05-23 15:41:01 +0530

    Add files via upload

---
+++ carrental/admin/includes/leftbar.php
@@ -0,0 +1,37 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Brands</a>
+<ul>
+<li><a href="create-brand.php">Create Brand</a></li>
+<li><a href="manage-brands.php">Manage Brands</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-car"></i> Vehicles</a>
+					<ul>
+						<li><a href="post-avehical.php">Post a Vehicle</a></li>
+						<li><a href="manage-vehicles.php">Manage Vehicles</a></li>
+					</ul>
+				</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> Bookings</a>
+					<ul>
+						<li><a href="new-bookings.php">New</a></li>
+						<li><a href="confirmed-bookings.php">Confirmed</a></li>
+						<li><a href="canceled-bookings.php">Canceled</a></li>
+					</ul>
+				</li>
+
+		
+
+				<li><a href="testimonials.php"><i class="fa fa-table"></i> Manage Testimonials</a></li>
+				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop"></i> Manage Conatct us Query</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Reg Users</a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
+
+			</ul>
+		</nav>
